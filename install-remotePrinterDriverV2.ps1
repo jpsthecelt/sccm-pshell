6bd29031a58b2d41a4efc84ee03b9b15510fa945
jpsthecsucelt <john.singer@csueastbay.edu>
2019-10-09 14:03:46 -0700

    Updated install-remotePrinterDriverV2 with appropriate local paths

--- install-remotePrinterDriverV2.ps1
+++ install-remotePrinterDriverV2.ps1
@@ -17,53 +17,57 @@ $DrvInf="hpshC42A4_x64.inf"
 $DrvName="HP Color Laserjet Pro M454dn -MI3094"
 $PrtIp="134.154.240.7"
 
-$    if ($DrvPath.length -eq 0 -or $DrvInf.length -eq 0 -or $DrvName.length -eq 0 -or $PrtIp.length -eq 0) {
+foreach ($uut in $ComputerName) {
+    if ($DrvPath.length -eq 0 -or $DrvInf.length -eq 0 -or $DrvName.length -eq 0 -or $PrtIp.length -eq 0) {
 	      write-error "Insufficent number of parameters"
 	      exit
-	    }
+	}
+	# Now, copy the files to the remote system....
+	Copy-Item -Path "$DrvPath" -Destination "\\$uut\c$\" -Recurse
 
 	foreach ($uut in $ComputerName) {Copy-Item -Path "$DrvPath" -Destination "\\$uut\c$\" -Recurse}
 	
     try {
-	Invoke-Command -ComputerName $uut -ScriptBlock { 
-	    if (Test-Path $DrvPath+"\\"+$DrvInf -eq $true) {
-	        $IP = $PrtIp
-	        $Driver = $DrvName
-	        $Description = "$DrvName (autoinstall)"
-	#Install Printer Driver
-	        $driverclass = [WMIClass]"Win32_PrinterDriver"
-	        $driverobj = $driverclass.createinstance()
-			$driverobj.Name="$Driver"
-			$uutPath = "c:\\"+$DrvPath.split("\\")[4]+"\\"
-			$driverobj.DriverPath = $uutPath
-			# "C:\Driver File"
-			$driverobj.Infname = $uutPath+$DrvInf
-			"C:\Driver File\Driver.inf"
-	        $newdriver = $driverclass.AddPrinterDriver($driverobj)
-	        $newdriver = $driverclass.Put()
-	#Install Printer Port
-	        $Port = ([wmiclass]"win32_tcpipprinterport").createinstance()
-	        $Port.Name = "$IP"
-	        $Port.HostAddress = "$IP"
-	        $Port.Protocol = "1"
-	        $Port.PortNumber = "9100"
-	        $Port.SNMPEnabled = $true
-	        $Port.Description = "$IP"
-	        $Port.Put()
-	#Install Printer
-	        $Printer = ([wmiclass]"win32_Printer").createinstance()
-	        $Printer.Name = "$Description"
-	        $Printer.DriverName = "$Driver"
-	        $Printer.DeviceID = "$Description"
-	        $Printer.Shared = $false
-	        $Printer.PortName = "$IP"
-	        $Port.Description = "$IP"
-	        $Printer.Put()
+		Invoke-Command -ComputerName $uut -ScriptBlock { 
+		    if (Test-Path $DrvPath+"\\"+$DrvInf -eq $true) {
+		        $IP = $PrtIp
+		        $Driver = $DrvName
+		        $Description = "$DrvName (autoinstall)"
+		#Install Printer Driver
+		        $driverclass = [WMIClass]"Win32_PrinterDriver"
+		        $driverobj = $driverclass.createinstance()
+				$driverobj.Name="$Driver"
+				$uutPath = "c:\\"+$DrvPath.split("\\")[4]+"\\"
+				$driverobj.DriverPath = $uutPath
+				# "C:\Driver File"
+				$driverobj.Infname = $uutPath+$DrvInf
+				# "C:\Driver File\Driver.inf"
+		        $newdriver = $driverclass.AddPrinterDriver($driverobj)
+		        $newdriver = $driverclass.Put()
+		#Install Printer Port
+		        $Port = ([wmiclass]"win32_tcpipprinterport").createinstance()
+		        $Port.Name = "$IP"
+		        $Port.HostAddress = "$IP"
+		        $Port.Protocol = "1"
+		        $Port.PortNumber = "9100"
+		        $Port.SNMPEnabled = $true
+		        $Port.Description = "$IP"
+		        $Port.Put()
+		#Install Printer
+		        $Printer = ([wmiclass]"win32_Printer").createinstance()
+		        $Printer.Name = "$Description"
+		        $Printer.DriverName = "$Driver"
+		        $Printer.DeviceID = "$Description"
+		        $Printer.Shared = $false
+		        $Printer.PortName = "$IP"
+		        $Port.Description = "$IP"
+		        $Printer.Put()
+		        }
 	        }
-        }
-		else {
-			Write-Output "File Does not exsist on $env:computername"}
-			} 
+			else {
+				Write-Output "File Does not exsist on $env:computername"}
+				} 
 	Catch {
 		write-host "Whoops!"
 	}
+}
